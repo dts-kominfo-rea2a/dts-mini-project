f405759ae369fd611610f4ec1b78d646b845fd31
luthfanzuf <luthfanzuf@gmail.com>
2022-07-15 23:49:05 +0700

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,106 @@
 import React from "react";
+import "./header.css"
+import mainLogo from '../assets/logo.png'
+import ProfilePicture3 from '../assets/ProfilePicture3.png'
 
 
-const header = () => {
-    return 
-    <>
-        <div>
+const Header = () => {
+    return (
+    <nav className="bg-black border-black-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+            <a href="https://flowbite.com/" className="flex items-center">
+                <img src={mainLogo} className="mr-3 h-6 sm:h-9" alt="Main Logo" />
+                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            </a>
+            <div className="flex md:order-2">
+                <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
+                    <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                    <span className="sr-only">Search</span>
+                </button>
+
+                {/* username */}
+                <div className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
+                    <p>Username</p>
+                </div>
+
+                {/* icon gifts */}
+                <button type="button" className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
+                    <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v13m0-13V6a2 2 0 1 1 2 2h-2Zm0 0V5.5A2.5 2.5 0 1 0 9.5 8H12Zm-7 4h14M5 12a2 2 0 1 1 0-4h14a2 2 0 1 1 0 4M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-7"></path></svg>
+                </button>
+
+                {/* icon notifications */}
+                <div className="">
+                    <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12 22c1.1 0 2-.9 2-2h-4a2 2 0 0 0 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z" clipRule="evenodd"></path></svg>
+                </div>
+
+                 {/* user avatar */}
+                 {/* <button type="button" className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"> */}
+                <div>
+                    <img  className="mr-3 h-6 sm:h-9" src={ProfilePicture3}></img>
+                </div>
+                {/* </button> */}
+
+                 {/* triangle button */}
+                 <button type="button" className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
+                    <svg className="w-5 h-5" viewBox="0 0 24 12" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.5927 11.2795L23.9452 0.250125C23.9722 0.229507 23.9908 0.199734 23.9974 0.166405C24.0039 0.133077 23.9981 0.0984877 23.981 0.069141C23.9639 0.0397943 23.9366 0.017712 23.9044 0.00704594C23.8721 -0.00362016 23.8371 -0.00213564 23.8059 0.0112211H0.194149C0.162909 -0.00213564 0.127867 -0.00362016 0.0956101 0.00704594C0.063353 0.017712 0.036103 0.0397943 0.0189841 0.069141C0.00186515 0.0984877 -0.00394355 0.133077 0.00264983 0.166405C0.0092432 0.199734 0.0277846 0.229507 0.0547879 0.250125L10.4073 11.2795C10.612 11.4965 10.8589 11.6693 11.1328 11.7874C11.4066 11.9055 11.7017 11.9664 12 11.9664C12.2983 11.9664 12.5934 11.9055 12.8672 11.7874C13.1411 11.6693 13.388 11.4965 13.5927 11.2795Z"  fill="currentColor"/>
+                    </svg>
+
+                </button>
+
+                <div className="w-10 h-5 text-gray-500"></div>
+                <div className="hidden relative md:block">
+                    <div className="hidden  flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                        <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                        <span className="sr-only">Search icon</span>
+                        
+                        
+                    </div>
+                    <input type="text" id="search-navbar" className="hidden block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+                    
+
+                    
+                    <div></div>
+                
+                </div>
+
+                {/* toggle menu untuk mobile view */}
+                <button data-collapse-toggle="navbar-search" type="button" className="hidden inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+                    <span className="sr-only">Open menu</span>
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                </button>
+
             
+
+            </div>
+            <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-search">
+            <div className="relative mt-3 md:hidden">
+                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                </div>
+                <input type="text" id="search-navbar" className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+            </div>
+            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                <li>
+                <a href="#" className="block py-2 pr-4 pl-3 text-white bg-white-700 rounded md:bg-transparent md:text-white-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                </li>
+                <li>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Series</a>
+                </li>
+                <li>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Movies</a>
+                </li>
+                <li>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">New and Popular</a>
+                </li>
+                <li>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">My List</a>
+                </li>
+            </ul>
+            </div>
         </div>
-    </>
+    </nav>
+    )
 }
+
+export default Header

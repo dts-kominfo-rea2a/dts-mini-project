cd63d902bf1857812ca13ceed4589dd372cc746c
PanduAldi <pratamapandu652@gmail.com>
2022-07-16 22:39:53 +0700

    delete .env

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import Container from "react-bootstrap/esm/Container";
 import { Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Trending from "./pages/Trending";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navigasi />
-
       <Container>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/trending" element={<Trending />} />
         </Routes>
       </Container>
+      <Footer />
     </>
   );
 }

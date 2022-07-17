08d1eec1ee36655ca060a9d11a70fe7722248005
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-07-17 20:42:25 +0700

    fix: change pattern for export object

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,54 @@
+import "./Footer.css";
+import { Container, Button } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <Container className="footer__container" maxWidth="lg">
+          <div className="footer__content">
+            <div className="footer__content__box">
+              <ul>
+                <li>Audio and Subtitles</li>
+                <li>Media Center</li>
+                <li>Security</li>
+                <li>Contact us</li>
+              </ul>
+            </div>
+            <div className="footer__content__box">
+              <ul>
+                <li>Audio Description</li>
+                <li>Investor Relations</li>
+                <li>Legal Provisions</li>
+              </ul>
+            </div>
+            <div className="footer__content__box">
+              <ul>
+                <li>Help center</li>
+                <li>Jobs</li>
+                <li>Cookie Preferences</li>
+              </ul>
+            </div>
+            <div className="footer__content__box">
+              <ul>
+                <li>Gift Cards</li>
+                <li>Terms of Use</li>
+                <li>Corporate Information</li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer__action">
+            <Button variant="outlined" color="success">
+              Service Code
+            </Button>
+          </div>
+          <div className="footer__copyright">
+            <span>&copy; 2022 Movies, All Right Reserved.</span>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Footer;

802e7de4accff3d0a316aeffab9f827ece65b5f1
goodwellsundawa <sundawawelly@gmail.com>
2022-07-18 18:48:49 +0700

    update 02

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Movie Apps</header>
     </div>
   );
 }

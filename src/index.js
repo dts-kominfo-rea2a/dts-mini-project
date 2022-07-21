0d8546a320994c5c340cea76c2591a783e667fc1
Yudi <rohmat.wahyudi@pintap.id>
2022-07-21 16:11:27 +0700

    new theme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// ** React Imports
+import { Suspense, lazy } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// ** Redux Imports
+import { store } from "./redux/store"
+import { Provider } from "react-redux"
+
+// ** ThemeColors Context
+
+import { ThemeContext } from "./utility/context/ThemeColors"
+
+// ** ThemeConfig
+import themeConfig from "./configs/themeConfig"
+
+// ** Toast
+import { Toaster } from "react-hot-toast"
+
+// ** Spinner (Splash Screen)
+import Spinner from "./@core/components/spinner/Fallback-spinner"
+
+// ** Ripple Button
+import "./@core/components/ripple-button"
+
+// ** PrismJS
+import "prismjs"
+import "prismjs/themes/prism-tomorrow.css"
+import "prismjs/components/prism-jsx.min"
+
+// ** React Perfect Scrollbar
+import "react-perfect-scrollbar/dist/css/styles.css"
+
+// ** React Hot Toast Styles
+import "@styles/react/libs/react-hot-toasts/react-hot-toasts.scss"
+
+// ** Core styles
+import "./@core/assets/fonts/feather/iconfont.css"
+import "./@core/scss/core.scss"
+import "./assets/scss/style.scss"
+
+// ** Service Worker
+import * as serviceWorker from "./serviceWorker"
+
+// ** Lazy load app
+const LazyApp = lazy(() => import("./App"))
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <Provider store={store}>
+      <Suspense fallback={<Spinner />}>
+        <ThemeContext>
+          <LazyApp />
+          <Toaster
+            position={themeConfig.layout.toastPosition}
+            toastOptions={{ className: "react-hot-toast" }}
+          />
+        </ThemeContext>
+      </Suspense>
+    </Provider>
+  </BrowserRouter>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()

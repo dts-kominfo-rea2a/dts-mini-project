08971bab9dc241a579d49d2551ef219ef5280e64
IrwanS99 <irwansanjaya7999@gmail.com>
2022-07-21 21:52:49 +0700

    move to github
    from my local to github

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://pbs.twimg.com/profile_images/1243623122089041920/gVZIvphd_400x400.jpg" alt="" /></Link>
+                <Link to="/movies/popular" style={{ textDecoration: "none" }}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{ textDecoration: "none" }}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{ textDecoration: "none" }}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
